b5724f3105eaf85f12ad67083f99f8a24c6a691d
Muhammad <asadullahnadeem48@gmail.com>
2025-09-09 22:51:55 +0530

    add code

---
+++ src/main/java/org/example/loginandsignupwithemailusingspringboot/LoginAndSignupWithEmailUsingSpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.example.loginandsignupwithemailusingspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginAndSignupWithEmailUsingSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginAndSignupWithEmailUsingSpringBootApplication.class, args);
+    }
+
+}
