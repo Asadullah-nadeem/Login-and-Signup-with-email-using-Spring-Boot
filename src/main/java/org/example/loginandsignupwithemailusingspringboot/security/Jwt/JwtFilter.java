733c0c3aa90e32bcc0c50f232669283d935ab493
Muhammad <asadullahnadeem48@gmail.com>
2025-09-09 23:52:49 +0530

    update and Add Code

--- src/main/java/org/example/loginandsignupwithemailusingspringboot/security/Jwt/JwtFilter.java
+++ src/main/java/org/example/loginandsignupwithemailusingspringboot/security/Jwt/JwtFilter.java
@@ -1,4 +1,61 @@
 package org.example.loginandsignupwithemailusingspringboot.security.Jwt;
 
-public class JwtFilter {
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.example.loginandsignupwithemailusingspringboot.repository.UserRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+    private final UserRepository userRepository;
+
+    public JwtFilter(JwtUtil jwtUtil, UserRepository userRepository) {
+        this.jwtUtil = jwtUtil;
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+
+
+        final String authHeader = request.getHeader("Authorization");
+        if(authHeader != null && authHeader.startsWith("Bearer ")){
+            String token = authHeader.substring(7);
+            try{
+                String email = jwtUtil.extractEmail(token);
+                userRepository.findByName(email).orElseThrow(
+                        () -> new RuntimeException("User not Found")
+                );
+
+                //TODO Valid Just Continue
+            }catch (ExpiredJwtException e){
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                response.getWriter().write("Token Expired");
+                return;
+            }catch (JwtException | IllegalArgumentException e){
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                response.getWriter().write("Invalid Token");
+                return;
+            }
+        }
+        filterChain.doFilter(request, response);
+
+    }
+
+    protected boolean shouldNotFilter(HttpServletRequest request){
+        String path = request.getRequestURI();
+        return path.startsWith("/api/auth/");
+    }
 }
